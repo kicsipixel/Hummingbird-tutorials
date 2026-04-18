da3a6be6566b6623f969e8719b52da8398804c5b
Szabolcs Tóth <tsz@purzelbaum.hu>
2026-04-18 19:36:13 +0200

    Introduced friendID.

--- Neo4j/splitwise/Sources/App/Controllers/FriendsController.swift
+++ Neo4j/splitwise/Sources/App/Controllers/FriendsController.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Hummingbird
 import Logging
 import SwiftCypher
@@ -10,9 +11,9 @@ struct FriendsController<Context: RequestContext> {
     group
       .post(use: create)
       .get(use: index)
-      .get("/:friend", use: show)
-      .patch("/:friend", use: edit)
-      .delete("/:friend", use: delete)
+      .get("/:friendID", use: show)
+      .patch("/:friendID", use: edit)
+      .delete("/:friendID", use: delete)
   }
 
   // MARK: - create
@@ -24,10 +25,16 @@ struct FriendsController<Context: RequestContext> {
   /// }'`
   @Sendable
   func create(_ request: Request, context: Context) async throws -> HTTPResponse.Status {
-    let friend = try await request.decode(as: Friend.self, context: context)
+    let friend = try await request.decode(as: Friend.Create.self, context: context)
 
     /// `CREATE (alice:FRIEND {name: 'Alice'})`
-    let queryRequest = QueryRequest(statement: "CREATE (n:FRIEND {name: $name})", parameters: ["name": .string("\(friend.name)")])
+    let queryRequest = QueryRequest(
+      statement: "CREATE (n:FRIEND {friendID: $friendID, name: $name})",
+      parameters: [
+        "friendID": .string(UUID().uuidString),
+        "name": .string("\(friend.name)"),
+      ]
+    )
     _ = try await client.runQuery(request: queryRequest)
 
     return .created
@@ -45,9 +52,11 @@ struct FriendsController<Context: RequestContext> {
     let response = try await client.runQuery(request: queryRequest)
     for row in response.rows {
       if let node = row["n"]?.nodeValue,
+        let friendIdString = node.properties["friendID"]?.stringValue,
+        let friendID = UUID(uuidString: friendIdString),
         let name = node.properties["name"]?.stringValue
       {
-        friends.append(Friend(name: name))
+        friends.append(Friend(friendID: friendID, name: name))
       }
     }
 
@@ -56,73 +65,77 @@ struct FriendsController<Context: RequestContext> {
 
   // MARK: - show
   /// Returns with a node with single `FRIEND` node
-  /// `curl "http://localhost:8080/api/v1/friends/Alice"`
+  /// `curl "http://localhost:8080/api/v1/friends/2DB325DC-6884-40EB-8ECB-8695641746DD"`
   @Sendable
   func show(_ request: Request, context: Context) async throws -> Friend {
-    let friend = try context.parameters.require("friend", as: String.self)
+    let friendID = try context.parameters.require("friendID", as: String.self)
 
-    /// `MATCH (n:FRIEND {name: 'Alice'}) RETURN n;`
+    /// `MATCH (n:FRIEND {friendID: '2DB325DC-6884-40EB-8ECB-8695641746DD'}) RETURN n;`
     let queryRequest = QueryRequest(
-      statement: "MATCH (n:FRIEND {name: $name}) RETURN n",
+      statement: "MATCH (n:FRIEND {friendID: $friendID}) RETURN n",
       parameters: [
-        "name": .string(friend)
+        "friendID": .string(friendID)
       ]
     )
 
     let response = try await client.runQuery(request: queryRequest)
     for row in response.rows {
-      if let node = row["n"]?.nodeValue, let name = node.properties["name"]?.stringValue {
-        return Friend(name: name)
+      if let node = row["n"]?.nodeValue,
+        let friendIdString = node.properties["friendID"]?.stringValue,
+        let friendID = UUID(uuidString: friendIdString),
+        let name = node.properties["name"]?.stringValue
+      {
+        return Friend(friendID: friendID, name: name)
       }
     }
-    throw HTTPError(.notFound, message: "The friend with name \(friend) was not found")
+    throw HTTPError(.notFound, message: "The friend with id:\(friendID) was not found")
   }
 
   // MARK: - edit
   /// Edits node
-  /// `curl -X "PATCH" "http://localhost:8080/api/v1/friends/Alice" \
+  /// `curl -X "PATCH" "http://localhost:8080/api/v1/friends/2DB325DC-6884-40EB-8ECB-8695641746DD" \
   ///     -H 'Content-Type: application/json' \
   ///     -d $'{
   ///  "name": "Emma"
   ///  }'`
   @Sendable
   func edit(_ request: Request, context: Context) async throws -> Friend {
-    let originalFriend = try context.parameters.require("friend", as: String.self)
-    let newFriend = try await request.decode(as: Friend.self, context: context)
+    let friendID = try context.parameters.require("friendID", as: String.self)
+    let newFriend = try await request.decode(as: Friend.Create.self, context: context)
 
-    /// `MATCH (n:FRIEND {name: 'Alice'}) SET n.name = 'Emma';`
+    /// `MATCH (n:FRIEND {friendID: '2DB325DC-6884-40EB-8ECB-8695641746DD'}) SET n.name = 'Emma';`
     let queryRequest = QueryRequest(
-      statement: "MATCH (n:FRIEND {name: $oldName}) SET n.name = $newName",
+      statement: "MATCH (n:FRIEND {friendID: $friendID}) SET n.name = $newName",
       parameters: [
-        "oldName": .string(originalFriend),
+        "friendID": .string(friendID),
         "newName": .string(newFriend.name),
       ]
     )
 
     let response = try await client.runQuery(request: queryRequest)
     guard response.counters?.propertiesSet ?? 0 > 0 else {
-      throw HTTPError(.notFound, message: "The friend with name \(originalFriend) was not found")
+      throw HTTPError(.notFound, message: "The friend with id:\(friendID) was not found")
     }
-    return Friend(name: newFriend.name)
+    return Friend(friendID: UUID(), name: newFriend.name)
   }
 
   // MARK: - delete
   /// Deletes a node
-  /// `curl -X "DELETE" "http://localhost:8080/api/v1/friends/Alice"`
+  /// `curl -X "DELETE" "http://localhost:8080/api/v1/friends/2DB325DC-6884-40EB-8ECB-8695641746DD"`
   @Sendable
   func delete(_ request: Request, context: Context) async throws -> HTTPResponse.Status {
-    let friend = try context.parameters.require("friend", as: String.self)
+    let friendID = try context.parameters.require("friendID", as: String.self)
 
-    /// MATCH (n:FRIEND {name: 'Alice'}) DETACH DELETE n;
+    /// MATCH (n:FRIEND {friendID: '2DB325DC-6884-40EB-8ECB-8695641746DD'}) DETACH DELETE n;
     let queryRequest = QueryRequest(
-      statement: "MATCH (n:FRIEND {name: $name}) DETACH DELETE n",
-      parameters: ["name": .string(friend)]
+      statement: "MATCH (n:FRIEND {friendID: $friendID}) DETACH DELETE n",
+      parameters: ["friendID": .string(friendID)]
     )
     let response = try await client.runQuery(request: queryRequest)
 
     // Check if the database has deleted the node
     guard response.counters?.nodesDeleted ?? 0 > 0 else {
-      throw HTTPError(.notFound, message: "The friend with name \(friend) was not found")
+      throw HTTPError(.notFound, message: "The friend with id:\(friendID) was not found")
     }
     return .noContent
   }
--- Neo4j/splitwise/Sources/App/Models/Friend.swift
+++ Neo4j/splitwise/Sources/App/Models/Friend.swift
@@ -1,7 +1,15 @@
+import Foundation
 import Hummingbird
 
 struct Friend: Codable {
+  let friendID: UUID
   let name: String
+}
+
+extension Friend {
+  struct Create: Codable {
+    let name: String
+  }
 }
 
 extension Friend: ResponseCodable {}
